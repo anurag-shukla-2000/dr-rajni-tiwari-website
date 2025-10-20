3e994fa97d9f0b8650873d0474acacc1eb4666b4
Emdient Techlab <emdtechlab@gmail.com>
2025-10-20 07:49:11 +0530

    fix: update for Pages Router configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,4 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      window.scrollTo(0, 0);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
-  return <Component {...pageProps} />;
+import '../styles/globals.css'
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
